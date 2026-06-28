b7e95ba8006efde83b0925c7b7e52d373a6ec99c
Samira12-dev <samiraelboussidi663@gmail.com>
2026-06-28 14:46:26 +0100

    making landing page responsive

--- Landing/src/components/Navbar.jsx
+++ Landing/src/components/Navbar.jsx
@@ -1,19 +1,44 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
+  const navRef = useRef();
+
+  const showNav = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
-    <section className="div">
-      <nav className="navbar">
-        <h2 className="logo">Glow <span className="lab">Lab</span></h2>
+    <header className="header">
+
+      <h2 className="logo">
+        Glow <span className="lab">Lab</span>
+      </h2>
+
+      <nav ref={navRef} className="navbar">
         <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#features">Features</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#testimonials">Testimonials</a></li>
-            <li><a href="#contact">Contact</a></li>
+          <li><a href="#home" onClick={showNav}>Home</a></li>
+          <li><a href="#features" onClick={showNav}>Features</a></li>
+          <li><a href="#about" onClick={showNav}>About</a></li>
+          <li><a href="#testimonials" onClick={showNav}>Testimonials</a></li>
+          <li><a href="#contact" onClick={showNav}>Contact</a></li>
+
+          <li>
             <button className="btn">Get Started</button>
+          </li>
         </ul>
+
+        <button className="nav-btn nav-close-btn" onClick={showNav}>
+          <FaTimes />
+        </button>
       </nav>
-    </section>
-  )
+
+      <button className="nav-btn nav-open-btn" onClick={showNav}>
+        <FaBars />
+      </button>
+
+    </header>
+  );
 }
+
 export default Navbar;
